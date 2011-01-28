418d172b410fb5a69e9950c7dab8bb3ca03ed82e
mbhxyz@gmail.com <mbhxyz@gmail.com@e93bb8a4-f497-72e0-c41f-53ae9b2856d2>
2011-01-28 03:36:06 +0000

    mostly blank skeleton

---
+++ Robot11/src/edu/arhs/first1100/manipulator/Manipulator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.arhs.first1100.manipulator;
+
+/**
+ *
+ * @author markbh
+ */
+public class Manipulator {
+
+}
