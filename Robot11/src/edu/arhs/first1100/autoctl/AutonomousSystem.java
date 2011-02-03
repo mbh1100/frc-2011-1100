d79ac1b525717d306479136788e676034e619e94
nicksober.no@gmail.com <nicksober.no@gmail.com@e93bb8a4-f497-72e0-c41f-53ae9b2856d2>
2011-02-03 23:46:05 +0000

    Updated Autonomous system

--- Robot11/src/edu/arhs/first1100/autoctl/AutonomousSystem.java
+++ Robot11/src/edu/arhs/first1100/autoctl/AutonomousSystem.java
@@ -1,18 +1,17 @@
 package edu.arhs.first1100.autoctl;
 
 import edu.arhs.first1100.util.SystemBase;
+import edu.arhs.first1100.line.LineSystem;
+import edu.arhs.first1100.drive.DriveSystem;
+import edu.arhs.first1100.camera.CameraSystem;
+import edu.arhs.first1100.manipulator.ManipulatorSystem;
+
 
 public class AutonomousSystem extends SystemBase
 {
     private final int STATE_IDLE = 0;                  // Doing nothing
-    private final int STATE_FIND_LINE = 1;
-    private final int STATE_FOLLOW_LINE = 2;
-    private final int STATE_LINE_DECISION = 3;         // When is come at a line intersection
-    private final int STATE_POSITION_HORIZONTALLY = 4; // transform drivetrain, position horizontally
-    private final int STATE_POSITION_LIFT = 5;         // Adjust arm height
-    private final int STATE_REACH_OUT_ARM = 6;         // Reaching out the arm
-    private final int STATE_PLACE_RING = 7;            // Releasing the claw to place the ring on the peg
-    private final int STATE_PULL_BACK_ARM = 8;         // Pulling back the arm
+    //private final int STATE_SCORE_RING = 0;
+
     
     private int state = 0;
     private boolean targetRack = false;
@@ -45,62 +44,14 @@ public class AutonomousSystem extends SystemBase
          * 6 7 8
          */
 
-        
-        if(state == STATE_IDLE)
-        {
-            targetRack = rack;
-            targetPeg = peg;
-            state = STATE_FOLLOW_LINE;
-        }
     }
 
-    public void tick()
+    public void run()
     {
-        switch(state)
-        {
-            case STATE_FIND_LINE:
-            {
-                /*
-                if lineSystem state == no line
-                    driveSystem.setDriveSpeed(0.5,0.5);
-                else
-                    state = STATE_FOLLOW_LINE
-                 */
-                break;
-            }
-            case STATE_FOLLOW_LINE:
-            {
-                            /*
-            if lineSystem state == too far left
-                driveSystem.setDriveSpeed(0.6,0.4);
+        LineSystem line = robot.lineSystem;
 
-            else if lineSystem state == too far right
-                driveSystem.setDriveSpeed(0.4,0.6);
-
-            else if lineSystem state == just right
-                driveSystem.setDriveSpeed(0.5,0.5);
-
-            else if lineSystem state == fork in the road
-                state = STATE_LINE_DECISION
-
-            else if lineSystem state == no line
-                state = STATE_POSITION_FOR_SCORING
-            */
-                break;
-            }
-            case STATE_LINE_DECISION:
-            {
-                if (targetRack == false) // move robot left a little
-                {
-                    state = STATE_FOLLOW_LINE;
-                }
-                else if (targetRack == true) // move robot right a little
-                {
-                    state = STATE_FOLLOW_LINE;
-                }
-                break;
-            }
-        }
         //robot.manipulatorSystem.lift.followY(robot.cameraSystem.getCenterY());
-    }    
+    }
+
+
 }
