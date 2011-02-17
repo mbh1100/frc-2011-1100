550f0ad3df01e9ee7b19d432a81feea6f32c75c2
chickenbellyfinn@gmail.com <chickenbellyfinn@gmail.com@e93bb8a4-f497-72e0-c41f-53ae9b2856d2>
2011-02-17 02:30:14 +0000

    fixes & liftToPeg class. <- wiil be working on more

---
+++ Robot11/src/edu/arhs/first1100/autoctl/LiftToPegRoutine.java
@@ -0,0 +1,29 @@
+/**
+ * Moves the Lift until it reaches a certain particle (top middle
+ * and open the template in the editor.
+ */
+
+package edu.arhs.first1100.autoctl;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.arhs.first1100.robot.RobotMain;
+import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
+
+/**
+ *
+ * @author akshay
+ */
+public class LiftToPegRoutine extends Routine
+{
+
+    public LiftToPegRoutine(RobotMain robot, int sleep)
+    {
+        super(robot, sleep);
+    }
+
+    public void tick()
+    {
+        ParticleAnalysisReport pegs  = robot.cameraSystem.getParticles();
+    }
+
+}
