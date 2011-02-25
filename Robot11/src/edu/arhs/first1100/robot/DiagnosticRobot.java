9dc241b387c436f10b8869ab8124633cd7d55624
34soccerplayer <34soccerplayer@e93bb8a4-f497-72e0-c41f-53ae9b2856d2>
2011-02-25 20:59:24 +0000

    diagnostic -Akshay

--- Robot11/src/edu/arhs/first1100/robot/DiagnosticRobot.java
+++ Robot11/src/edu/arhs/first1100/robot/DiagnosticRobot.java
@@ -1,6 +1,7 @@
 package edu.arhs.first1100.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Solenoid;
 
 /**
@@ -12,52 +13,73 @@ public class DiagnosticRobot
     //Channel numbers
     final int LEFT_JOYSTICK_CHANNEL = 1;
     final int RIGHT_JOYSTICK_CHANNEL = 2;
-    final int XBOX_CONTROLLER_CHANNEL = 3;
+    //Drive System
     final int RIGHT_JAG_1 = 1;
     final int RIGHT_JAG_2 = 3;
     final int LEFT_JAG_1 = 2;
     final int LEFT_JAG_2 = 4;
+    //Manipulator Jags
     final int ARM_JAG = 8;
     final int LIFT_JAG = 6;
+    //Solenoids
     final int WRIST = 2;
     final int CLAW = 1;
-    //XBOX controller channel
+    //XBOX controller channels
+    final int XBOX_CONTROLLER_CHANNEL = 3;
     final int XBOX_LEFT_STICK_Y = 2;
     final int XBOX_RIGHT_STICK_Y = 5;
     final int XBOX_LEFT_BUMBER = 5;
     final int XBOX_RIGHT_BUMBER = 6;
+    //Minbot
+    final int MINIBOT_SIDECAR = 5;
+    final int MINIBOT_ARM_VICTOR = 1;
+    final int MINIBOT_BELT_VICTOR = 2;
+    final int MINIBOT_TOGGLE_BUTTON = 6;
+    final int MINIBOT_DEPLOY_AXIS = 1;
     
     Joystick left, right, xbox;
     Jaguar r1, r2, l1, l2, arm, lift;
     Solenoid wrist, claw;
+    Victor mbArm, mbBelt;
     boolean lastWristState, lastClawState;
+    boolean minibotActive = false;
 
 
     public DiagnosticRobot()
-    {
+    {        
+        //Control init
         left = new Joystick(LEFT_JOYSTICK_CHANNEL);
         right = new Joystick(RIGHT_JOYSTICK_CHANNEL);
         xbox = new Joystick(XBOX_CONTROLLER_CHANNEL);
+        //Drive init
         r1 = new Jaguar(RIGHT_JAG_1);
         r2 = new Jaguar(RIGHT_JAG_2);
         l1 = new Jaguar(LEFT_JAG_1);
         l2 = new Jaguar(LEFT_JAG_2);
+        //Manipulator init
         arm = new Jaguar(ARM_JAG);
         lift = new Jaguar(LIFT_JAG);
         wrist = new Solenoid(WRIST);
         lastWristState = wrist.get();
         claw = new Solenoid(CLAW);
         lastClawState = claw.get();
+        //Minibot init
+        mbArm = new Victor(MINIBOT_SIDECAR, MINIBOT_ARM_VICTOR);
+        mbBelt = new Victor(MINIBOT_SIDECAR, MINIBOT_BELT_VICTOR);
     }
     
     public void teleop()
     {
         log("TELEOP CYCLE");
+        minibotActive = left.getRawButton(MINIBOT_TOGGLE_BUTTON);
         //Driving
-        l1.set(left.getY());
-        l2.set(left.getY());
-        r1.set(right.getY());
-        r2.set(right.getY());
+        if(!minibotActive)
+        {
+            l1.set(left.getY());
+            l2.set(left.getY());
+            r1.set(right.getY());
+            r2.set(right.getY());
+        }
         //Manipulator
         arm.set(xbox.getRawAxis(XBOX_LEFT_STICK_Y));
         lift.set(-xbox.getRawAxis(XBOX_RIGHT_STICK_Y));
@@ -81,13 +103,16 @@ public class DiagnosticRobot
         {
             lastWristState = false;
         }
-
-
-
-
+        //Minibot
+        if(minibotActive)
+        {
+            mbArm.set(left.getRawAxis(MINIBOT_DEPLOY_AXIS));
+            mbBelt.set(right.getRawAxis(MINIBOT_DEPLOY_AXIS));
+        }
     }
 
-    public void disable(){
+    public void disable()
+    {
         log("Disabled");
     }
 
@@ -100,6 +125,4 @@ public class DiagnosticRobot
     {
         System.out.println("DIAGNOSTIC:" + message);
     }
-
-
 }
