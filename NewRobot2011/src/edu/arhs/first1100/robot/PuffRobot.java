add2369bc23ed73ebaad0a37341edc51087491bf
henk5066 <henk5066@e93bb8a4-f497-72e0-c41f-53ae9b2856d2>
2011-02-25 20:14:46 +0000

    blah

--- NewRobot2011/src/edu/arhs/first1100/robot/PuffRobot.java
+++ NewRobot2011/src/edu/arhs/first1100/robot/PuffRobot.java
@@ -18,17 +18,152 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class PuffRobot extends SimpleRobot {
+{
+    public AutonomousSystem autonomousSystem;
+    public CameraSystem cameraSystem;
+    public DriveSystem driveSystem;
+    public LineSystem lineSystem;
+    public ManipulatorSystem manipulatorSystem;
+    public MinibotSystem minibotSystem;
+    public OperatorSystem operatorSystem;
+    public Compressor compressor;
+
+    public DiagnosticRobot diagnosticRobot;
+    private DigitalInput diagSwitch;
+
+    boolean diagnostic = false;
     /**
-     * This function is called once each time the robot enters autonomous mode.
+     *
      */
-    public void autonomous() {
-        
+    public void robotInit()
+    {
+        diagSwitch = new DigitalInput(13);
+        diagnostic = !diagSwitch.get();
+        if(!diagnostic)
+        {
+            log("+------------------------+");
+            log("| USING REGULAR ROBOT    |");
+            log("+------------------------+");
+            autonomousSystem = new AutonomousSystem(this, 100);
+            operatorSystem = new OperatorSystem(this, 100);
+
+            cameraSystem = new CameraSystem(this, 100);
+            driveSystem = new DriveSystem(this, 100);
+            lineSystem = new LineSystem(this, 10000);
+            manipulatorSystem = new ManipulatorSystem(this, 100);
+            minibotSystem = new MinibotSystem(this, 100);
+        }
+        else if (diagnostic)
+        {
+            log("+------------------------+");
+            log("| USING DIAGNOSTIC ROBOT |");
+            log("+------------------------+");
+            diagnosticRobot = new DiagnosticRobot();
+        }
+        compressor = new Compressor(6, 1);
+        compressor.start();
+
+        // operator & autonomous threads started/stopped by teleopInit and autonomousInit
+
+    }
+
+    // Periodic = called when driver station data is updated
+    // Continuous = called as fast as possible
+
+    /**
+     *
+     */
+    public void autonomous()
+    {
+        log("AUTONOMOUS");
+
+        if(!diagnostic)
+        {
+            operatorSystem.stop();
+            autonomousSystem.start();
+
+            cameraSystem.start();
+            manipulatorSystem.start();
+            driveSystem.start();
+            lineSystem.start();
+            minibotSystem.start();
+        }
+        else if (diagnostic)
+        {
+            log("DIAGNOSTIC AUTONOMOUS");
+        }
     }
 
     /**
-     * This function is called once each time the robot enters operator control.
+     *
      */
-    public void operatorControl() {
+    public void operatorControl()
+    {
+        if (!diagnostic){
 
+            log("Enabling teleop");
+
+            log("Starting opsys");
+            operatorSystem.start();
+
+            log("stopping ausys");
+            autonomousSystem.stop();
+
+            log("Starting cam, drive, manip");
+            cameraSystem.start();
+            manipulatorSystem.start();
+            driveSystem.start();
+            lineSystem.start();
+            minibotSystem.start();
+
+            log("Enabled teleop");
+        }
+        else if (diagnostic)
+        {
+            while(isOperatorControl())
+            {
+                diagnosticRobot.teleop();
+                Timer.delay(.1);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public void disabled()
+    {
+        if (!diagnostic)
+        {
+            log("Enabling disabling...");
+
+            log("stopping opsys");
+            operatorSystem.stop();
+
+            log("stopping ausys");
+            autonomousSystem.stop();
+
+            log("stopping cam, line, manip, and drive");
+            cameraSystem.stop();
+            manipulatorSystem.stop();
+            driveSystem.stop();
+            lineSystem.stop();
+            minibotSystem.stop();
+
+            log("disabled");
+        }
+        else if(diagnostic)
+        {
+            diagnosticRobot.disable();
+        }
+    }
+
+    /**
+     *
+     * @param message
+     */
+    public void log(String message)
+    {
+        System.out.println("RobotMain: " + message);
     }
 }
