b3bb88880afce1c540fde80e9647c44c52115d58
nicksober.no@gmail.com <nicksober.no@gmail.com@e93bb8a4-f497-72e0-c41f-53ae9b2856d2>
2011-03-29 01:27:24 +0000

    ObjectOrientedOperatorControl

---
+++ NewRobot2011/src/edu/arhs/first1100/oopctl/JoystickHandler.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.arhs.first1100.oopctl;
+
+public class JoystickHandler
+{
+
+}
---
+++ NewRobot2011/src/edu/arhs/first1100/oopctl/XboxJoystick.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.arhs.first1100.oopctl;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.arhs.first1100.util.SystemBase;
+
+public class XboxJoystick extends SystemBase
+{
+    private Button button_a;
+    private Joystick xboxJoystick;
+    public XboxJoystick(int channel)
+    {
+        super();
+        xboxJoystick = new Joystick(channel);
+        button_a = new Button(xboxJoystick, 1);
+        this.start();
+    }
+
+    public void tick()
+    {
+        button_a.update();
+    }
+}
